be516e2585fe208d72b42163618fc669f1943b3a
cihsgame <JamesSmith13378@protonmail.com>
2025-08-25 16:56:46 -0400

    add map logic + movement

--- src/game/map/mapLogic.js
+++ src/game/map/mapLogic.js
@@ -40,10 +40,40 @@ function generateMap(width, height) {
                 type: terrain,
                 isVisible: false,
                 isExplored: false,
-                danger: Math.floor(distance)
+                danger: Math.floor(distance),
+                settlement: 0,
+                roadLevel: 0
             };
         }
     }
+    
+    //generate the highway recursively. takes in x and y, modifies either the left or upper tile
+    function generateHighway(x,y) {
+
+        map[x][y].roadLevel = 2;
+
+        if (x == 0 && y == 0) {
+            return;
+        }
+
+        if (x == 0) {
+            generateHighway(x,y-1);
+        }
+        else if (y == 0) {
+            generateHighway(x-1,y);
+        } else {
+            let roll = Math.random();
+            if (roll < 0.5) {
+                generateHighway(x,y-1);
+            } else {
+                generateHighway(x-1,y);
+            }
+        }
+        
+    }
+
+    function 
+
     
     return {
         width: width,
@@ -51,5 +81,25 @@ function generateMap(width, height) {
         tiles: map
     };
 }
+
+function generateVillage(x,y,terrain) {
+
+    var village = {
+        name: "Village",
+        population: 0,
+        x: x,
+        y: y,
+        tradeLevel: 0,
+        innLevel: 0,
+        leaderLevel: false,
+        friendliness: 0
+    };
+
+    return village;
+
+
+}
+
+
 
 export { generateMap };
